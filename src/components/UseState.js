3e608f922b7b8dcacb117a1cf00b9f51b12d4c25
dloyolar <dieegolr@gmail.com>
2022-01-21 12:33:37 -0300

    feat: add security code validations in UseState

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,15 +1,22 @@
 import { useEffect, useState } from 'react';
 
+const SECURITY_CODE = 'qwerty';
+
 export const UseState = ({ name }) => {
+  const [value, setValue] = useState('');
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (loading) {
+      setError(false);
       setTimeout(() => {
+        if (value !== SECURITY_CODE) setError(true);
+
         setLoading(false);
       }, 3000);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading]);
 
   return (
@@ -22,7 +29,14 @@ export const UseState = ({ name }) => {
 
       {loading && <p>Loading...</p>}
 
-      <input type="text" placeholder="Security Code" />
+      <input
+        type="text"
+        placeholder="Security Code"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
 
       <button onClick={() => setLoading(true)}>Check</button>
     </div>
